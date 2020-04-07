5cecd40fbae1903555629c4919bbfc76f5df3b8c
NotSandy <sliu55@buffalo.edu>
2020-04-06 21:33:10 -0400

    Added Database Functionality + Temp User + Reports
    - app.js: added MongoDB as required and created a users collection which includes 1 user which is used for the whole duration of phase 2

--- routes/database.js
+++ routes/database.js
@@ -4,6 +4,15 @@ const mongo = require('mongodb');
 const monk = require('monk');
 
 let db = monk('mongo:27017/familychat');
-let userCollection = db.get('users');
+
+// let userCollection = db.get('users');
+// var temp_user = {
+//     id: "1",
+//     name: {
+//         first: "John",
+//         last: "Doe"},
+//     family_id: "1"
+// };
+// userCollection.insert(temp_user);
 
 module.exports = router;
